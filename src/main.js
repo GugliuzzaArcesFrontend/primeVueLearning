237888abe800c748c677fd8e7f862ba340cc0443
Daniele Gugliuzza <gruppostudioarces@gmail.com>
2024-09-20 11:31:52 +0200

    Sistemato css checklist (bottoni, hover, responsivity)

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css';
 import App from './App.vue';
 
 import PrimeVue from 'primevue/config';
-// import Aura from '@primevue/themes/aura';
+import Aura from '@primevue/themes/aura';
 import Lara from '@primevue/themes/lara';
 import 'primeicons/primeicons.css';
 import ToastService from 'primevue/toastservice';
@@ -12,7 +12,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const app = createApp(App);
 
-app.use(PrimeVue, { theme: { /* unstyled: true, */ preset: Lara } });
+app.use(PrimeVue, { theme: { /* unstyled: true, */ preset: Aura } });
 app.use(ToastService);
 
 app.mount('#app');
